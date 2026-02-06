e1abdf4ed05ea18d67cf760cb31a7797873205b5
Mansi Chaudhary <your_email@example.com>
2026-02-06 16:24:05 +0530

    components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = (ob) => {
+  return (
+    <div>
+      <h1>{ob.year[2]} is doing B.tech in {ob.company.estdyear} </h1>
+    </div>
+  )
+}
+
+export default Footer
